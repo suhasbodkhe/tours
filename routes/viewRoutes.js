669ad868ab21f6f1ec1b6cc1a35bdc212d45bd9b
Suhas <suhas.bodkhe@gmail.com>
2021-04-21 18:13:44 +0530

    Alert middleware function corrected

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,7 +5,7 @@ const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.use(viewsController.alerts);
+router.use(viewsController.alert);
 
 router.get(
   '/',
